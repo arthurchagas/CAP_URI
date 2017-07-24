3d39e4090f2421e02a5969a3cb9bde577c35325b
arthurchagas <744344@comp.ufscar.br>
2017-07-24 18:37:08 -0300

    [C] 1001

---
+++ OBG 01/1001.c
@@ -0,0 +1,20 @@
+/*
+ * Autor: Luiz Arthur Chagas Oliveira       RA: 744344
+ * Revisor: Luiz Arthur Chagas Oliveira       RA: 744344
+ * Data: 18/04/2017
+ *
+ * Objetivo: URI 1001
+ */
+#include <stdio.h>
+
+int main() {
+    int A, B, sum;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    sum = A + B;
+
+    printf("X = %d\n",sum);
+
+    return 0;
+}
