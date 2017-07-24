defc09115be25a1d3d63aafeb3acfba1b4df2080
arthurchagas <744344@comp.ufscar.br>
2017-07-24 18:55:54 -0300

    [C] 1174

---
+++ OBG 08/1174.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double X[100];
+    int i;
+
+    for (i = 0; i < 100; i++) {
+        scanf("%lf", &X[i]);
+        if (X[i] <= 10)
+            printf("A[%d] = %.1lf\n", i, X[i]);
+    }
+
+    return 0;
+}
