d0203c186b9806d43233ed2c4bc3285e3ca5f36c
arthurchagas <744344@comp.ufscar.br>
2017-07-24 18:37:54 -0300

    [C] 1004

---
+++ OBG 01/1004.c
@@ -0,0 +1,20 @@
+/*
+ * Autor: Luiz Arthur Chagas Oliveira       RA: 744344
+ * Revisor: Luiz Arthur Chagas Oliveira       RA: 744344
+ * Data: 18/04/2017
+ *
+ * Objetivo: URI 1004
+ */
+#include <stdio.h>
+
+int main() {
+    int int1, int2, prod;
+
+    scanf("%d", &int1);
+    scanf("%d", &int2);
+    prod = int1 * int2;
+
+    printf("PROD = %d\n", prod);
+
+    return 0;
+}
